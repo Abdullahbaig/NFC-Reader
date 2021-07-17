a1d81fe30e99b2449622937295c27192b1fe0cf3
Abdullah Baig <abdullahbaigbappy@gmail.com>
2021-07-17 22:41:53 +0600

    Add project files.

---
+++ NFC Reader/NFC Reader/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NFC_Reader
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadButton_Clicked(object sender, EventArgs e)
+        {
+
+            DisplayAlert("Reading Tag", "The Tag is: ", "Close");
+        }
+
+        private void WriteButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
